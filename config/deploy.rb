4ed2a5e1ff317fb65448a8c1f392d20b95285144
Steven Xu <steven@stevenxu.ca>
2011-06-28 19:15:49 -0400

    added rvmrc

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,13 +16,9 @@ set :deploy_port, 5555
 set :rvm_type, :user
 set :rvm_ruby_string, '1.9.2'
 
-after 'deploy:update_code', 'deploy:make_production_symlinks'
 after 'deploy', 'deploy:cleanup'
 
 namespace :deploy do
-  task :make_production_symlinks do
-    run "ln -s #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-  end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "cd #{current_path} && bundle exec thin stop"
     run "cd #{current_path} && bundle exec thin start -p #{deploy_port} -a 127.0.0.1 -e production -d"
