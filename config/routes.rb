b3d8f2db697be09cd7fea6f5c21375b79ddeb7db
Steven Xu <steven@stevenxu.ca>
2011-07-06 12:01:43 -0400

    added calendar redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Stevenxu::Application.routes.draw do
 
   devise_for :users
 
+  match 'calendar' => redirect('http://www.google.com/calendar/embed?src=steven%40stevenxu.ca')
+
   root :to => 'pages#index'
 end
